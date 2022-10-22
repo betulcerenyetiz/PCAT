aeca92e25da4392a2b310c00549516292e0fb9b6
Betül Ceren YETİZ <betulcerenyetiz@gmail.com>
2022-10-22 20:14:50 +0300

    added new changes and temp folders

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-const fn = () => {
-  console.log('Function working')
-}
+const express = require("express")
+const ejs =require("ejs")
+const app= express()
+
+//TEMPLATE ENGINE
+app.set("view engine","ejs")
+
+//MIDDLEWARES
+app.use(express.static("public"))
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+app.get("/about",(req,res)=>{
+    res.render("about")
+})
+app.get("/add",(req,res)=>{
+    res.render("add")
+})
+
+const port=5000
+app.listen(port,() =>{
+    console.log(`Sunucu ${port} portunda başlatıldı...`)
+})
