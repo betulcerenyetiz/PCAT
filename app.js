ab6d67dc9bab6276f9bcb083e746be7f250634e1
Betül Ceren YETİZ <betulcerenyetiz@gmail.com>
2022-10-27 11:47:42 +0300

    added update photo

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const fileUpload = require('express-fileupload')
+const methodOverride = require('method-override')
 const ejs =require("ejs")
 const fs =require("fs")
 const Photo =require("./models/Photo")
 
 const app= express()
 
+
 //connect DB
 mongoose.connect('mongodb://localhost/pcat-db')
 
+
 //TEMPLATE ENGINE
 app.set("view engine","ejs")
+
 
 //MIDDLEWARES
 app.use(express.static("public"))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(fileUpload())
+app.use(methodOverride('_method'))
 
 
 //ROUTES
@@ -39,6 +44,13 @@ app.get("/photos/:id", async (req,res)=>{
         photo
     })
 })
+app.get("/photos/edit/:id", async (req,res)=>{
+    const photo = await Photo.findOne({_id:req.params.id})
+    res.render("edit",{
+        photo
+    })
+})
+
 
 //post with form action to db
 app.post("/photos", async (req,res)=>{
@@ -59,6 +71,15 @@ app.post("/photos", async (req,res)=>{
         res.redirect("/")
     })
     
+})
+
+//update with put request
+app.put("/photos/:id", async (req,res)=>{
+    const photo = await Photo.findOne({_id:req.params.id})
+    photo.title=req.body.title
+    photo.description=req.body.description
+    photo.save()
+    res.redirect(`/photos/${req.params.id}`)
 })
 
 const port=5000
